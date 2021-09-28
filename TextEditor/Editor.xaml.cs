56516b4792e968032550bd39f3a8ab976bee9718
niklas-hjelm <88381305+niklas-hjelm@users.noreply.github.com>
2021-09-28 16:00:08 +0200

    Images

---
+++ TextEditor/Editor.xaml.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for Editor.xaml
+    /// </summary>
+    public partial class Editor : UserControl
+    {
+        public Editor()
+        {
+            InitializeComponent();
+            FontFamilyBox.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            FontSizeBox.ItemsSource = new List<double>()
+            { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void FontFamilyBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (TextField == null)
+            {
+                return;
+            }
+            //TODO: fix when none selected and only apply on new text.
+
+            if (e.Source is ComboBox box && box.SelectedItem != null)
+            {
+                TextField.Selection.ApplyPropertyValue(TextElement.FontFamilyProperty, box.SelectedItem);
+            }
+        }
+
+        private void FontSizeBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextField.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, FontSizeBox.Text);
+        }
+
+        private void TextField_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            object temp = TextField.Selection.GetPropertyValue(TextElement.FontWeightProperty);
+            BoldToggle.IsChecked = (temp != DependencyProperty.UnsetValue) && temp.Equals(FontWeights.Bold);
+            temp = TextField.Selection.GetPropertyValue(TextElement.FontStyleProperty);
+            ItalicToggle.IsChecked = (temp != DependencyProperty.UnsetValue) && temp.Equals(FontStyles.Italic);
+            temp = TextField.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            UnderlineToggle.IsChecked = (temp != DependencyProperty.UnsetValue) && temp.Equals(TextDecorations.Underline);
+
+            temp = TextField.Selection.GetPropertyValue(TextElement.FontFamilyProperty);
+            FontFamilyBox.SelectedItem = temp;
+            temp = TextField.Selection.GetPropertyValue(TextElement.FontSizeProperty);
+            FontSizeBox.Text = temp.ToString();
+        }
+
+        private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(TextField.Document.ContentStart, TextField.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(TextField.Document.ContentStart, TextField.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+    }
+}
--- TextEditor/MainWindow.xaml.cs
+++ TextEditor/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TextEditor
 {
@@ -20,110 +15,10 @@ namespace TextEditor
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static List<FontFamily> fonts = new List<FontFamily>() {
-            new FontFamily("Courier New"),
-            new FontFamily("Arial"),
-            new FontFamily("Constantia")
-    };
 
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        private void FontFamilyBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            if (e.Source is ComboBox box)
-            {
-                TextField.Selection.ApplyPropertyValue(FontFamilyProperty, fonts[box.SelectedIndex]);
-            }
-        }
-
-        private void BoldToggle_Checked(object sender, RoutedEventArgs e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            TextField.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
-        }
-
-        private void BoldToggle_Unchecked(object sender, RoutedEventArgs e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            TextField.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Normal);
-        }
-
-        private void ItalicToggle_Checked(object sender, RoutedEventArgs e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            TextField.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
-        }
-
-        private void ItalicToggle_Unchecked(object sender, RoutedEventArgs e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            TextField.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Normal);
-        }
-
-        private void UnderlineToggle_Checked(object sender, RoutedEventArgs e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            TextField.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
-        }
-
-        private void UnderlineToggle_Unchecked(object sender, RoutedEventArgs e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            TextField.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
-        }
-
-        private void FontSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (TextField == null)
-            {
-                return;
-            }
-            //TODO: fix when none selected and only apply on new text.
-
-            if (e.Source is Slider slider)
-            {
-                TextField.Selection.ApplyPropertyValue(FontSizeProperty, slider.Value);
-            }
-        }
-      
     }
 }
